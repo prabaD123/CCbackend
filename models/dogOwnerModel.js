8945374989f22ee5267fc2eb4c7cbc000e301724
prabaD123 <prabanidevindya97@gmail.com>
2023-10-23 03:04:08 +0530

    change code

--- models/dogOwnerModel.js
+++ models/dogOwnerModel.js
@@ -51,64 +51,10 @@ const dogOwnerSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-…
- const mongoose = require('mongoose');
-
-const dogOwnerSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  phoneNumber: {
-    type: String,
-  },
-
-   password: {
-    type: String,
-    required: true
-},
-
-  dogs: [
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      breed: {
-        type: String,
-        required: true,
-      },
-      age: {
-        type: Number,
-      },
-      description: {
-        type: String,
-      },
-    },
-  ],
-  location: {
-    type: String,
-  },
-  bio: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
     default: Date.now,
   },
 });
 
 const DogOwner = mongoose.model('DogOwner', dogOwnerSchema);
 
-module.exports = DogOwner;
+module.exports = DogOwner
