b5718c9d18e2f8362c2683adf1fc12bfaaab1e1a
prabaD123 <prabanidevindya97@gmail.com>
2023-10-23 02:59:20 +0530

    change code

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -1,1 +1,23 @@
+const userService = require('./dogService');
 
+const createDogControllerFn = async (req,res) =>
+{
+    try{
+        console.log(req.body);
+        const status = await dogServive.createDogControllerFn(req,body);
+        console.log(status);
+
+        if(status) {
+            res.send({"status": true, "message": "Account created successfully"});
+        } else {
+            res.send({"status": false, "message": "Error !"})
+        }
+    }
+    catch(err)
+{
+    console.log(err);
+}
+}
+
+
+module.exports = { createDogControllerFn };
